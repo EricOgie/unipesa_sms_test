07cf56210261c00835bb4c97a4e80667202ce48b
Ericogia <ogia.aghahowa@gmail.com>
2021-10-21 04:13:04 +0100

    Finsh

--- index.php
+++ index.php
@@ -6,43 +6,42 @@ include_once 'Router.php';
 
 $router = new Router(new Request);
 
-
 $router->post('/send-sms', function ($request) {
 
     $input = $request->getBody();
 
-    if (($input['phone'][0] !== '0')) {
-        http_response_code(422);
-        return json_encode([
-            'error' => 'invalid phone',
-            'message' => 'Phone must be 11 digit and must start with 0'
-        ]);
+    if ($input['phone'][0] === '0' && strlen($input['phone']) == 11) {
+        $phoneWithCode = $input['country_code'] . substr($input['phone'], 1);
+
+        $payload = [
+            'messages' => [
+                [
+                    "channel" => "sms",
+                    "to" => $phoneWithCode,
+                    "content" => "Unipesa Test-1234"
+                ]
+            ]
+        ];
+
+        $headerData = [
+            "Content-Type: application/json", "Accept: application/json", "Authorization: wu2gnmtaT_iHe_a-9GJOdg=="
+        ];
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, 'https://platform.clickatell.com/v1/message');
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headerData);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($payload));
+
+
+        $result = curl_exec($curl);
+        curl_close($curl);
+        return $result;
     }
 
-    $phoneWithCode = $input['country_code'] . substr($input['phone'], 1);
-
-    $payload = [
-        'messages' => [
-            [
-                "channel" => "sms",
-                "to" => $phoneWithCode,
-                "content" => "Unipesa Test-1234"
-            ]
-        ]
-    ];
-
-    $headerData = [
-        "Content-Type: application/json", "Accept: application/json", "Authorization: wu2gnmtaT_iHe_a-9GJOdg=="
-    ];
-
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, 'https://platform.clickatell.com/v1/message');
-    curl_setopt($curl, CURLOPT_POST, 1);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headerData);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($payload));
-
-
-    $result = curl_exec($curl);
-    curl_close($curl);
-    return $result;
+    http_response_code(422);
+    return json_encode([
+        'error' => 'invalid phone',
+        'message' => 'Phone must be 11 digit and must start with 0'
+    ]);
 });
