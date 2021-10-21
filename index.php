986887018712e8e74af5620302857c29b9770146
Ericogia <ogia.aghahowa@gmail.com>
2021-10-21 03:06:06 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+
+include_once 'Request.php';
+include_once 'Router.php';
+
+
+$router = new Router(new Request);
+
+$router->get('/', function () {
+    return 'HELLO NOW';
+});
+
+$router->post('/send-sms', function ($request) {
+
+    $input = $request->getBody();
+
+
+    if (($input['phone'][0] !== '0')) {
+        http_response_code(422);
+        return json_encode([
+            'error' => 'invalid phone',
+            'message' => 'Phone must be 11 digit and must start with 0'
+        ]);
+    }
+
+    $phoneWithCode = $input['country_code'] . substr($input['phone'], 1);
+
+    $payload = [
+        'messages' => [
+            [
+                "channel" => "whatsapp",
+                "to" => $phoneWithCode,
+                "content" => "Unipesa Test-1234"
+            ],
+            [
+                "channel" => "sms",
+                "to" => $phoneWithCode,
+                "content" => "Unipesa Test-1234"
+            ]
+        ]
+    ];
+
+    // return json_encode($payload);
+
+    $headerData = [
+        "Content-Type: application/json", "Accept: application/json", "Authorization: wu2gnmtaT_iHe_a-9GJOdg=="
+    ];
+
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, 'https://platform.clickatell.com/v1/message');
+    curl_setopt($curl, CURLOPT_POST, 1);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headerData);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($payload));
+    // curl_setopt($curl, CURL_RETURNTRANSFER, true);
+
+    $result = curl_exec($curl);
+    curl_close($curl);
+    return $result;
+
+
+
+
+
+
+
+    return  $phoneWithCode;
+});
